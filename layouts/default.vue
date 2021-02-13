4e34d5541215c8606fb69eb685b2cb72758180cc
💩 <69432552+unchidev@users.noreply.github.com>
2021-02-13 11:52:31 +0900

    GitHubリンク追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,12 @@ div
       p
         | 提供：
         a(href="https://koeda.me/", target="_blank" rel="noopener noreferrer") 肥溜め
+
+      p
+        | ソースコード：
+        a(href="https://github.com/koedame/zerotimer.koeda.me", target="_blank" rel="noopener noreferrer")
+          b-icon(icon="github")
+          | GitHub
 </template>
 
 <script>
